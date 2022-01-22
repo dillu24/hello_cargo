68e9699ea6cba8b1959d9bfaa901a2c9247fd35c
Dylan Galea <dylan@simply-vc.com.mt>
2022-01-22 16:45:18 +0100

    Learnt about types, borrowing and mutability.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,31 @@
+use std::io;
+
+fn test(var: &mut i32) {
+    *var = 5;
+    println!("{}", var);
+}
+
 fn main() {
-    println!("Hello, world!");
+    let a = [1, 2, 3, 4, 5];
+
+    println!("Please enter an array index");
+
+    let mut index = String::new();
+    io::stdin()
+        .read_line(&mut index)
+        .expect("Failed to read line");
+    let index: usize = index
+        .trim()
+        .parse()
+        .expect("Index entered was not a number");
+
+    let element = a[index];
+
+    println!(
+        "The value of the element at index {} is: {}",
+        index, element
+    );
+
+    let mut var = 4;
+    test(&mut var);
 }
